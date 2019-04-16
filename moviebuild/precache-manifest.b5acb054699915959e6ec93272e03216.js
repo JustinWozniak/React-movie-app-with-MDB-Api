c72e410ef271e7fc55edf4fe16b962778be498df
Justin Wozniak <43418633+JustinWozniak@users.noreply.github.com>
2019-04-16 00:38:05 -0400

    Add files via upload

---
+++ moviebuild/precache-manifest.b5acb054699915959e6ec93272e03216.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ee7af28d26ecbb6f96fe02b8201aa5a",
+    "url": "/static/media/foxlogo.9ee7af28.jpg"
+  },
+  {
+    "revision": "acf230efc8463aecdfe6c79e608e26b5",
+    "url": "/static/media/banner.acf230ef.jpg"
+  },
+  {
+    "revision": "b5d71eb0608f711063981e6728eac681",
+    "url": "/static/media/background.b5d71eb0.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "002af90d27b81c0f3376",
+    "url": "/static/js/main.a21e34a2.chunk.js"
+  },
+  {
+    "revision": "3ba5aaa99193684c9c97",
+    "url": "/static/js/2.a22e5490.chunk.js"
+  },
+  {
+    "revision": "002af90d27b81c0f3376",
+    "url": "/static/css/main.3818a5e9.chunk.css"
+  },
+  {
+    "revision": "f2697163f0247db54cc61da64a798041",
+    "url": "/index.html"
+  }
+];
